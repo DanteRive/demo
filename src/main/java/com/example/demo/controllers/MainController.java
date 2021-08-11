6ce5c40b8596520dfc8f07bc5321097340919ac3
Roman <54954154+DanteRive@users.noreply.github.com>
2021-08-11 17:09:49 +0300

    Добавление регистрации, авторизации

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,11 +1,21 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Role;
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
 
 @Controller
 public class MainController {
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -16,5 +26,26 @@ public class MainController {
     public String about(Model model) {
         model.addAttribute("title", "About us");
         return "about";
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+
+        return "redirect:/login";
     }
 }
